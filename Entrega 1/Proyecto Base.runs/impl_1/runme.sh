ebc9c6a8c90094ded1c21dfc7458b461ab9fda0f
nacosta-cl <nlacosta@uc.cl>
2015-09-14 00:23:30 -0300

    v1.5
    Agregada Autoevaluacion
    
    Bugs:
    Hay un registro en alu declarado, sin usar

--- Entrega 1/Proyecto Base.runs/impl_1/runme.sh
+++ Entrega 1/Proyecto Base.runs/impl_1/runme.sh
@@ -41,7 +41,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .init_design.begin.rst
+/bin/touch .write_bitstream.begin.rst
 EAStep vivado -log Basys3.vdi -applog -m64 -messageDb vivado.pb -mode batch -source Basys3.tcl -notrace
 
 
