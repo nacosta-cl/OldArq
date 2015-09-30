2fe8439d005b831052e66661a2b6e6235bf45441
nacosta-cl <nlacosta@uc.cl>
2015-09-30 15:13:49 -0300

    v 0.0.1
    - Agregados archivos base del proyecto.

---
+++ Entrega 2/Proyecto Base.srcs/sources_1/new/Basys3.vhd
@@ -0,0 +1,148 @@
+--ALU básica
+--v1.1
+--IIC2343 - Arquitectura de computadores
+--Integrantes
+--  Nicolás Acosta Huenulef
+--  Sebastian Carraedo
+--  Benjamin Rigonesi
+--  Jorge Trincado
+--Entidad raiz
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Basys3 is
+    Port (
+        sw          : in   std_logic_vector (15 downto 0);
+        btn         : in   std_logic_vector (4 downto 0);  -- 0 Center, 1 Up, 2 Left, 3 Right, 4 Down
+        clk         : in   std_logic;
+        clk_up      : in   std_logic;
+        seg         : out  std_logic_vector (7 downto 0);
+        an          : out  std_logic_vector (3 downto 0)
+          );
+end Basys3;
+
+architecture Behavioral of Basys3 is
+
+component Clock_Divider
+    Port ( clk          : in std_logic;
+           clk_up       : in std_logic;
+           slow         : in std_logic;
+           clock        : out std_logic);
+    end component;
+    
+component Led_Driver
+    Port (  dis_a       : in   std_logic_vector (3 downto 0);
+            dis_b       : in   std_logic_vector (3 downto 0);
+            dis_c       : in   std_logic_vector (3 downto 0);
+            dis_d       : in   std_logic_vector (3 downto 0);
+            clk         : in   std_logic;
+            seg         : out  std_logic_vector (7 downto 0);
+            an          : out  std_logic_vector (3 downto 0)
+           );
+    end component;
+component ALU
+    Port ( numA        : in  std_logic_vector (15 downto 0);
+       numB        : in  std_logic_vector (15 downto 0);
+       sel      : in  std_logic_vector (2 downto 0);
+       ci       : in  std_logic;
+       co       : out std_logic;
+       res   : out std_logic_vector (15 downto 0));
+    end component;
+    
+component Reg
+    Port ( clock    : in  std_logic;
+           load     : in  std_logic;
+           up       : in  std_logic;
+           down     : in  std_logic;
+           datain   : in  std_logic_vector (15 downto 0);
+           dataout  : out std_logic_vector (15 downto 0));
+end component;
+
+signal num1 : STD_LOGIC_VECTOR (15 downto 0);
+signal num2 : STD_LOGIC_VECTOR (15 downto 0);
+signal nums : STD_LOGIC_VECTOR (15 downto 0);
+
+signal res : STD_LOGIC_VECTOR (15 downto 0);
+
+signal display : STD_LOGIC_VECTOR (15 downto 0);
+
+signal nulo : STD_LOGIC;
+--
+signal reg1 : STD_LOGIC_VECTOR (15 downto 0);
+signal reg2 : STD_LOGIC_VECTOR (15 downto 0);
+
+-- Integrados: reloj, display
+signal clock  : std_logic;                     
+            
+signal dis_a : std_logic_vector(3 downto 0);
+signal dis_b : std_logic_vector(3 downto 0);
+signal dis_c : std_logic_vector(3 downto 0);
+signal dis_d : std_logic_vector(3 downto 0);
+
+begin
+
+--Listado de instancias
+--Componentes basicos
+inst_Clock_Divider: Clock_Divider port map(
+        clk         =>clk,
+        clk_up      =>clk_up,
+        slow        =>'0',
+        clock       =>clock
+    );
+
+inst_Led_Driver: Led_Driver port map(
+		dis_a => dis_a,
+		dis_b => dis_b,
+        dis_c => dis_c,
+		dis_d => dis_d,
+        clk => clk,
+        seg => seg,
+        an => an
+	);
+--Registros. No se les entrega un datain, solo los cambios
+--Se mueve con izq-der
+inst_regA: Reg port map(
+        clock    => clock,
+        load     => '0',
+        up       => btn(3),
+        down     => btn(2),
+        datain   =>"0000000000000000",
+        dataout  => num1
+    );
+--Se mueve con up-down
+inst_regB: Reg port map(
+        clock    => clock,
+        load     => '0',
+        up       => btn(1),
+        down     => btn(4),
+        datain   => "0000000000000000",
+        dataout  => num2
+     );
+         
+--ALU op. Siempre conectado. Operacion de salida elegida mediante un mux
+inst_ALU: ALU port map
+(
+    numA => num1,
+    numB => num2,
+    sel => sw(2 downto 0),
+    ci => '0',
+    co => nulo,
+    res => res
+);
+--Fin de instancias
+
+--Selector de información
+with btn(0) select
+    display <=  nums when '0',
+                res when '1',
+                "0000000000000000" when others;
+
+--Creacion del vector para numeros
+nums (7 downto 0) <= num2 (7 downto 0);
+nums (15 downto 8) <= num1 (7 downto 0);
+--Envío a los numeros led
+dis_a <= display(15 downto 12);
+dis_b <= display(11 downto 8);
+dis_c <= display(7 downto 4);
+dis_d <= display(3 downto 0);
+end Behavioral;
---
+++ Entrega 2/Proyecto Base.srcs/sources_1/new/SUS16b.vhd
@@ -0,0 +1,61 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 09/11/2015 07:55:25 PM
+-- Design Name: 
+-- Module Name: SUS16b - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity SUS16b is
+    Port ( rWord1 : in STD_LOGIC_VECTOR (15 downto 0);
+           rWord2 : in STD_LOGIC_VECTOR (15 downto 0);
+           rest : out STD_LOGIC_VECTOR (15 downto 0));
+end SUS16b;
+
+architecture Behavioral of SUS16b is
+
+component ADD16b
+    Port ( word1 : in STD_LOGIC_VECTOR (15 downto 0);
+           word2 : in STD_LOGIC_VECTOR (15 downto 0);
+           sCin : in STD_LOGIC;
+           sSum : out STD_LOGIC_VECTOR (15 downto 0);
+           sCout : out STD_LOGIC);
+    end component;
+
+begin
+
+
+ADD: ADD16b port map(
+        word1 => rWord1,
+        word2 => not(rWord2),
+        sCin => '1',
+        sSum => rest
+    );
+
+
+end Behavioral;
