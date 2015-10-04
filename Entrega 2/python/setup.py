0064b9358b5fb0a61c425460663e0188ca165270
bregonesi <rego_1994@hotmail.com>
2015-10-04 19:14:32 -0300

    v1.1
    Agregado los exe e instruciones como compilar

---
+++ Entrega 2/python/setup.py
@@ -0,0 +1,16 @@
+from cx_Freeze import setup, Executable
+
+
+setup(
+    name = "Compilador" ,
+    version = "1" ,
+    description = "Compilador" ,
+    executables = [Executable("4096.py")]  ,
+)
+
+'''
+Hay que instalar cx_freeze y ejecutar en terminal
+	C:\Python34\python setup.py build
+Donde
+	C:\Python34\python -> directorio donde esta python
+	setup.py -> script
