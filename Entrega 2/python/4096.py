53d990685a50fc24c27ce40b1ae083a2e82cf0cb
bregonesi <rego_1994@hotmail.com>
2015-10-04 15:35:48 -0300

    v1.0.0
    Falta compilar el .exe nomas

--- Entrega 2/python/4096.py
+++ Entrega 2/python/4096.py
@@ -3,7 +3,6 @@ from tkinter import filedialog
 root = Tk()
 
 
-
 def sumBin(dec):
     if(dec[-1]=="d"):
         dec = int(dec[:-1])
@@ -68,46 +67,45 @@ lista = {
     'ADD A,B' : '0001011',
     'ADD B,A' : '0001100',
     'ADD A,Lit' : '0001101',
-    'ADD B,Lit' : '1111111', ##temporal
+    'ADD B,Lit' : '1000000',
     'ADD A,(Dir)' : '0001110',
-    'ADD B,(Dir)' : '1111111', ##temporal
+    'ADD B,(Dir)' : '1000001',
     'ADD (Dir)' : '0010000',
 
     'SUB A,B' : '0010001',
     'SUB B,A' : '0010010',
-    'SUB A,Lit' : '1111111', ##temporal
-    'SUB B,Lit' : '1111111', ##temporal
+    'SUB A,Lit' : '1000010',
+    'SUB B,Lit' : '1000011',
     'SUB A,(Dir)' : '0010011',
-    'SUB B,(Dir)' : '1111111', ##temporal
+    'SUB B,(Dir)' : '1000100',
     'SUB (Dir)' : '0010101',
 
     'AND A,B' : '0010110',
     'AND B,A' : '0010111',
     'AND A,Lit' : '0011000',
-    'AND B,Lit' : '1111111', ##temporal
+    'AND B,Lit' : '1000101',
     'AND A,(Dir)' : '0011001',
-    'AND B,(Dir)' : '1111111', ##temporal
+    'AND B,(Dir)' : '1000101',
     'AND(Dir)' : '0011011',
-
 
     'OR A,B' : '0011100',
     'OR B,A' : '0011101',
     'OR A,Lit' : '0011110',
-    'OR B,Lit' : '1111111', ##temporal
+    'OR B,Lit' : '1000111',
     'OR A,(Dir)' : '0011111',
-    'OR B,(Dir)' : '1111111', ##temporal
+    'OR B,(Dir)' : '1001000',
     'OR (Dir)' : '0100001',
 
     'NOT A' : '0100010',
     'NOT B,A' : '0100011',
-    'NOT (Dir),A' : '1111111', ##temporal
+    'NOT (Dir),A' : '1010100',
 
     'XOR A,B' : '0101000',
     'XOR B,A' : '0101001',
     'XOR A,Lit' : '0101010',
-    'XOR B,Lit' : '1111111', ##temporal
+    'XOR B,Lit' : '1001001',
     'XOR A,(Dir)' : '0101011',
-    'XOR B,(Dir)' : '1111111', ##temporal
+    'XOR B,(Dir)' : '1001010',
     'XOR (Dir)' : '0101101',
 
     'SHL A' : '0101110',
@@ -118,15 +116,15 @@ lista = {
     'SHR B,A' : '0110101',
     'SHR (Dir),A' : '0111001',
 
-    'INC A' : '1111111', ##temporal
+    'INC A' : '11111110', ##sin uso
     'INC B' : '0111010',
-    'INC (Dir)' : '1111111', ##temporal
+    'INC (Dir)' : '1001011',
 
-    'DEC A' : '1111111', ##temporal
+    'DEC A' : '11111111', ##sin uso
 
     'CMP A,B' : '0111011',
     'CMP A,Lit' : '0111100',
-    'CMP A,(Dir)' : '1111111', ##temporal
+    'CMP A,(Dir)' : '1001100',
 
     'JMP Ins' : '0111101',
     'JEQ Ins' : '0111110',
@@ -136,7 +134,7 @@ lista = {
     'JLT Ins' : '1000010',
     'JLE Ins' : '1000011',
     'JCR Ins' : '1000100',
-    'NOP' : '1111111' ##temporal
+    'NOP' : '1111111'
 }
 #print(lista)
 instrucciones = {}
@@ -195,8 +193,6 @@ def variablesIns(var):
         valor = valor[:-1]
     return ["MOV A,"+str(valor)+"b","MOV ("+str(variables[nombre])+"),A"] #transforma data a instruciones
 
-
-
 def contador(Linea):
     conta = 0
     for i in Linea:
@@ -239,22 +235,21 @@ def Leer(Archivo, label):
                         if cc == primeras_lineas[cccc]:
                             #label[Nombre].append(w[0].strip())
                             l=w[0].strip()
-                            label[Nombre].append(l.split(" ")[0]+" "+"".join(l.split(" ")[1:]))
+                            ins = l.split(" ")[0]+" "+"".join(l.split(" ")[1:])
+                            if(ins=="DEC A"):
+                                ins = "SUB A,1"
+                            if(ins=="INC A"):
+                                ins = "ADD A,1"
+                            label[Nombre].append(ins)
                         else:
                             label["CODE"].append(w[0].strip())
-
-
-
-
                     else:
                         break
     Ar.close()
 
-
 def orden_instrucciones(diccionario,valor,cantidad):
     if len(l)< cantidad:
         if valor != '':
-
             l.append(valor)
             x = valor.split()
             if len(x)>=2:
@@ -262,7 +257,6 @@ def orden_instrucciones(diccionario,valor,cantidad):
                     for z in diccionario[x[1]]:
                         if z != '' :
                             orden_instrucciones(diccionario,z,cantidad)
-
 
 def lista_instrucciones(diccionario,cantidad):
     for x in diccionario["CODE"]:
@@ -276,7 +270,6 @@ def suma_instrucciones(diccionario, orden_labels):
                 if x != '':
                     contador+= 1
     return contador
-
 
 instrucciones2 = {} #Dict con data con instrucciones
 def dataFinal(diccionario):
@@ -329,7 +322,6 @@ def instr_binario(inst):
                 return('decimal der')
     else:
         return("instruccion")
-
 
 def tipoVar(valor):
     if(not esint(valor[0])):
@@ -444,7 +436,6 @@ def dataFinalBin(dict):
 def rellenaLista(lista,N):
     for i in range(len(lista), N):
         lista.append(Rellena("",33))
-
 
 def outputTXT(file):
     f = open(file,'w+')
